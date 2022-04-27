d62e0dd99010eb29231512a342e0f4521a1f28e3
daniellePie <gdpeaceminusone@sjtu.edu.cn>
2022-04-27 22:03:30 +0800

    delete back_end codes

--- musicforum/src/main/java/musicForum/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package musicForum.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import musicForum.bean.users;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserMapper extends BaseMapper<users> {
-}
